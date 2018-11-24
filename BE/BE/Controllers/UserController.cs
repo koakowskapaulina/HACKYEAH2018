3a2a5b34ecbbfbc23e3650f5ab7a8058a23bb1bb
Izabela Antoniuk <iantoniuk@lbpro.pl>
2018-11-24 17:13:51 +0100

    merge

---
+++ BE/BE/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BE.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/User")]
+    public class UserController : Controller
+    {
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            var mockService = new MockService();
+            var cities = mockService.InitCities();
+
+            var city = cities.Where(x => x.CityID.Equals(id));
+
+            return city.ToString();
+        }
+    }
+}
