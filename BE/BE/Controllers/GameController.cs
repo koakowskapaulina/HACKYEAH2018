328533309f197fa847dd6fe233a5df0c5e212527
Adam Pietrowski <achaspokonara@gmail.com>
2018-11-24 20:22:35 +0100

    amazing raffle system

---
+++ BE/BE/Controllers/GameController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BE.Services;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BE.Controllers
+{
+    [Route("chcem/losowanko")]
+    public class GameController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public string Get()
+        {
+            var raffleService = new RaffleService();          
+
+            return JsonConvert.SerializeObject(raffleService.DoRaffle());
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ BE/BE/Services/RaffleService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BE.Services
+{
+    public class RaffleService
+    {
+        public IEnumerable<int> DoRaffle()
+        {
+            var resultList = new List<int>();
+
+            int id = 0;
+            Random rnd = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                id = rnd.Next(1, 20);
+                while (resultList.Contains(id))
+                {
+                    id = rnd.Next(1, 20);
+                }
+                resultList.Add(id);
+            }
+
+            return resultList;
+        }
+    }
+}
